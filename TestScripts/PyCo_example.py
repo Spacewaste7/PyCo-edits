7eea1683e477d278a8ed1e05a9d7f22ce74d85ec
bv1 <brettvelasquez@yahoo.com>
2018-06-06 12:43:46 -0500

    session file created from succesful PyContact run, PyCo_example.py had wrong segid 2. changes to PyCo_example.py for changed segid 2.

--- TestScripts/PyCo_example.py
+++ TestScripts/PyCo_example.py
@@ -1,6 +1,6 @@
 from PyContact.core.Scripting import PyContactJob, JobConfig
 
-job = PyContactJob("/home/bdv1/Desktop/rpn11_ubq.psf", "/home/bdv1/Desktop/rpn11_ubq.dcd", "attemptPdbDcd1", JobConfig(5.0, 2.5, 120, [0,0,1,1,0], [0,0,1,1,0], "segid UBQ", "segid RP11"))
+job = PyContactJob("/home/bdv1/Desktop/rpn11_ubq.psf", "/home/bdv1/Desktop/rpn11_ubq.dcd", "attemptPdbDcd1", JobConfig(5.0, 2.5, 120, [0,0,1,1,0], [0,0,1,1,0], "segid UBQ", "segid RN11"))
 
 job.runJob(4)
 
