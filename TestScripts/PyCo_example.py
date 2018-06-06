2a5519436ea482ddbf320837c4c0511c1988a9bb
bv1 <brettvelasquez@yahoo.com>
2018-06-06 11:49:51 -0500

    testing git commit
    	new file:   Biochemistry.py
    	new file:   Biochemistry.pyc
    	new file:   ContactAnalyzer.py
    	new file:   ContactAnalyzer.pyc
    	new file:   ContactFilters.py
    	new file:   DataHandler.py
    	new file:   DataHandler.pyc
    	new file:   LoadConfiguration.py
    	new file:   LogPool.py
    	new file:   LogPool.pyc
    	new file:   Scripting.py
    	new file:   Scripting.pyc
    	new file:   TestScripts/PyCo_example.py
    	new file:   __init__.py
    	new file:   __init__.pyc
    	new file:   aroundPatch.py
    	new file:   aroundPatch.pyc
    	new file:   multi_trajectory.py
    	new file:   multi_trajectory.pyc

---
+++ TestScripts/PyCo_example.py
@@ -0,0 +1,8 @@
+from PyContact.core.Scripting import PyContactJob, JobConfig
+
+job = PyContactJob("/home/bdv1/Desktop/rpn11_ubq.psf", "/home/bdv1/Desktop/rpn11_ubq.dcd", "attemptPdbDcd1", JobConfig(5.0, 2.5, 120, [0,0,1,1,0], [0,0,1,1,0], "segid UBQ", "segid RP11"))
+
+job.runJob(4)
+
+job.writeSessionToFile()
+                         
